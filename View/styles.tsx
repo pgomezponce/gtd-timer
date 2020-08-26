97fd3437382a33789a768b2803a13f5c7da18b62
pgomezponce <pgomezponce@gmail.com>
2020-08-26 16:11:28 +0200

    Stylesheet creation

---
+++ View/styles.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native"
+
+
+export const gtd_stylesheet = StyleSheet.create({
+    container:{
+        height:'100%',
+        justifyContent:'space-around',
+        alignItems:'center',
+    },
+    textCenter:{
+        textAlign:'center',
+        textAlignVertical:'center',
+    },
+
+    formContainer:{
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    upperPart:{
+        justifyContent:'center',
+        alignItems:'center'
+    }
+});
