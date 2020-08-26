1f0104630941aa3177933d51c2a14d990feeda46
pgomezponce <pgomezponce@gmail.com>
2020-08-26 16:11:48 +0200

    Started onBoarding

--- View/WorkScreen/components/WorkonBoarding.tsx
+++ View/WorkScreen/components/WorkonBoarding.tsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
-
+import { View } from 'react-native'
+import {Text, Button} from 'react-native-elements'
+import { names } from '../WorkScreen'
+import { gtd_stylesheet } from '../../styles'
 export default class WorkonBoarding extends Component {
     render() {
         return (
-            <View>
-                <Text> textInComponent </Text>
+            <View style={gtd_stylesheet.container}>
+                <View>
+                <Text h2 style={gtd_stylesheet.textCenter}>Let's begin</Text>
+                <Text h1 style={gtd_stylesheet.textCenter}>SPRINT</Text>
+                </View>
+                <Button type='solid' title='Start!' onPress={() => this.props.navigation.navigate(names.form)}/>
             </View>
         )
     }
 }
+
+
