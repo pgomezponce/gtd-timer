356d8ae8ced477e4860732ba583de0104ae86d97
pgomezponce <pgomezponce@gmail.com>
2020-08-27 20:11:31 +0200

    Updates

---
+++ precache-manifest.5e51c850803aff07b1114d12b5008cff.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b14b24868c96e5efb38",
+    "url": "pgomezponce.github.io/gtd-timer/static/js/app.796bed1f.chunk.js"
+  },
+  {
+    "revision": "02a38de7c67b2309b054",
+    "url": "pgomezponce.github.io/gtd-timer/static/js/runtime~app.06f74cef.js"
+  },
+  {
+    "revision": "ceb343beaddd38f83f5a",
+    "url": "pgomezponce.github.io/gtd-timer/static/js/2.9a6ec290.chunk.js"
+  },
+  {
+    "revision": "004c2bbb035d8d06bb830efc4673c886",
+    "url": "pgomezponce.github.io/gtd-timer/static/media/star.69c71386.png"
+  },
+  {
+    "revision": "2327736b3ea09c41abfb69be1221f430",
+    "url": "pgomezponce.github.io/gtd-timer/static/media/heart.23b5c2c0.png"
+  },
+  {
+    "revision": "51671417ef20e0bbc32f0a2bc6edfa95",
+    "url": "pgomezponce.github.io/gtd-timer/static/media/rocket.d2f3bf41.png"
+  },
+  {
+    "revision": "a7b9dc9de5f8f1fb1afbef917619a5ac",
+    "url": "pgomezponce.github.io/gtd-timer/static/media/bell.cf04af27.png"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "pgomezponce.github.io/gtd-timer/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "pgomezponce.github.io/gtd-timer/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "pgomezponce.github.io/gtd-timer/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "pgomezponce.github.io/gtd-timer/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "pgomezponce.github.io/gtd-timer/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "pgomezponce.github.io/gtd-timer/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "pgomezponce.github.io/gtd-timer/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "pgomezponce.github.io/gtd-timer/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "pgomezponce.github.io/gtd-timer/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "pgomezponce.github.io/gtd-timer/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "pgomezponce.github.io/gtd-timer/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "pgomezponce.github.io/gtd-timer/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "6ed49f90156069132b316be9b6f8ac18",
+    "url": "pgomezponce.github.io/gtd-timer/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "pgomezponce.github.io/gtd-timer/serve.json"
+  },
+  {
+    "revision": "f328c398676e066fb4e76f0c028592b4",
+    "url": "pgomezponce.github.io/gtd-timer/expo-service-worker.js"
+  },
+  {
+    "revision": "e65d83c0cb52ae586c60d95755fdf75f",
+    "url": "pgomezponce.github.io/gtd-timer/register-service-worker.js"
+  },
+  {
+    "revision": "b5d254d0c30e94782a183e03a17cefe5",
+    "url": "pgomezponce.github.io/gtd-timer/static/js/2.9a6ec290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71033553ddc0843407efe54c23890fe2",
+    "url": "pgomezponce.github.io/gtd-timer/favicon-16.png"
+  },
+  {
+    "revision": "c941078418f3ad7bb94a9ca94bc8b3dc",
+    "url": "pgomezponce.github.io/gtd-timer/favicon-32.png"
+  },
+  {
+    "revision": "615280e83775685e49117729b227d0fc",
+    "url": "pgomezponce.github.io/gtd-timer/favicon.ico"
+  },
+  {
+    "revision": "0e871e65dbc7fce8c9db59385922fb6e",
+    "url": "pgomezponce.github.io/gtd-timer/manifest.json"
+  }
+];
